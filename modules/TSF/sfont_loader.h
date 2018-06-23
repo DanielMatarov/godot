f92f5dfc51b65949abe76a9bb4c5bfd966df3ea1
Daniel Matarov <DMatarov@outlook.com>
2018-06-23 15:52:46 +0100

    resource loader attempts, getting unresolved external symbol error in register types.cpp

--- modules/TSF/sfont_loader.h
+++ modules/TSF/sfont_loader.h
@@ -4,13 +4,15 @@
 #include "io/resource_loader.h"
 
 class ResourceFormatLoaderSfont : public ResourceFormatLoader {
-	virtual RES load(const String &p_path, const String &p_original_path, Error *r_error = NULL);
+public:
+	virtual RES load(const String &filename, Error *r_error = NULL);
 	virtual void get_recognized_extensions(List<String> *p_extensions) const;
 	virtual bool handles_type(const String &p_type) const;
 	virtual String get_resource_type(const String &p_path) const;
 
 	ResourceFormatLoaderSfont();
 	virtual ~ResourceFormatLoaderSfont() {}
+	
 };
 
 #endif
