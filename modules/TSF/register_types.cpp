8ae8fceeb279a860982fca44407af9b5dc5369ab
Daniel Matarov <DMatarov@outlook.com>
2018-07-05 16:04:53 +0100

    working on adding midi files as a resource for MidiStream

--- modules/TSF/register_types.cpp
+++ modules/TSF/register_types.cpp
@@ -4,6 +4,8 @@
 #include "MidiStream.h"
 #include "MidiStreamPlayback.h"
 #include "sfont_loader.h"
+#include "midi_file_reader.h"
+#include "midi_loader.h"
 
 
 
@@ -11,11 +13,15 @@ void register_TSF_types() {
 
 #ifdef TOOLS_ENABLED
 	Ref<ResourceImporterSfont> sfont_import;
+	Ref<ResourceImporterMidi> midi_import;
+	midi_import.instance();
 	sfont_import.instance();
 		ResourceFormatImporter::get_singleton()->add_importer(sfont_import);
+		ResourceFormatImporter::get_singleton()->add_importer(midi_import);
 #endif     
 		ClassDB::register_class<MidiStream>();
         ClassDB::register_class<MidiStreamPlayback>();
+		ClassDB::register_class<MidiFileReader>();
 }
 
 void unregister_TSF_types() {
