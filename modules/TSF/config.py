b6d74fc591eb6b616e7ccb9195b3045f62d21e00
Daniel Matarov <DMatarov@outlook.com>
2018-07-25 16:43:12 +0100

    moved xml file to the tsf module folder

--- modules/TSF/config.py
+++ modules/TSF/config.py
@@ -4,4 +4,12 @@ def can_build(platform):
     return True
 
 def configure(env):
-    pass                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+    pass
+
+def get_doc_classes():
+    return [
+        "MidiStream"
+    ]
+
+def get_doc_path():
+    return "doc_classes"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
